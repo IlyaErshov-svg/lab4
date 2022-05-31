f6976aa46f3231995d45763f40a1c1074cdd0500
IlyaErshov-svg <isershov@miem.hse.ru>
2022-05-31 23:21:40 +0300

    Добавление абстрактного класса Product

--- Product.h
+++ Product.h
@@ -7,11 +7,17 @@ public:
 	ProductInfo(const std::string& info) {
 		m_info = info;
 	}
-	std::string get_info() {
+	std::string get_info() const{
 		return m_info;
 	}
 };
 class Product {
 public:
-	Product();
+	Product(const ProductInfo& product_info) {
+		m_product_info = product_info.get_info();
+	}
+	virtual std::string get_info() = 0;
+	virtual double get_cost() = 0;
+protected:
+	std::string m_product_info;
 };
