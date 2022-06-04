fafa4b61b7821608aef12bdefc7b141cc0712da0
IlyaErshov-svg <isershov@miem.hse.ru>
2022-06-04 18:57:16 +0300

    Добавление классов Position, AmountPosition, WeightPosition

---
+++ Position.h
@@ -0,0 +1,46 @@
+#include "Product.h"
+#pragma once
+
+class Position {
+protected:
+	Product* m_ptr_product;
+public:
+	Position(Product& product) {
+		m_ptr_product = &product;
+	}
+	Product* get_ptr_product() {
+		return m_ptr_product;
+	}
+	virtual double get_cost() = 0;
+	virtual double get_quantity() = 0;
+};
+
+class AmountPosition : public Position {
+private:
+	std::size_t m_amount;
+public:
+	AmountPosition(AmountProduct& amount_prod, std::size_t amount):Position(amount_prod) {
+		m_amount = amount;
+	}
+	double get_cost() override {
+		return m_ptr_product->get_cost();
+	}
+	double get_quantity() override{
+		return m_amount * m_ptr_product->get_cost();
+	}
+};
+
+class WeightPosition : public Position {
+private:
+	std::size_t m_weight;
+public:
+	WeightPosition(WeightProduct& weight_prod, std::size_t weight) : Position(weight_prod) {
+		m_weight = weight;
+	}
+	double get_cost() override {
+		return m_ptr_product->get_cost();
+	}
+	double get_quantity() override {
+		return m_weight * m_ptr_product->get_cost();
+	}
+};
